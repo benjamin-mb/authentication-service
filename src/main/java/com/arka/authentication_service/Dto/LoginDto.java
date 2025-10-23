faff9a21b097e81c908dc83384ad373d381ef860
user <benjaminmunera0@gmail.com>
2025-10-22 22:54:10 -0500

    change db

--- src/main/java/com/arka/authentication_service/Dto/LoginDto.java
+++ src/main/java/com/arka/authentication_service/Dto/LoginDto.java
@@ -2,8 +2,7 @@ package com.arka.authentication_service.Dto;
 
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.stereotype.Component;
+
 
 @NoArgsConstructor
 @Data
